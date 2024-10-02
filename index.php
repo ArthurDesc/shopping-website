e0b660a6929af7f33adc97831eff552c877c8485
Diomande225 <168726585+Diomande225@users.noreply.github.com>
2024-10-02 13:23:57 +0200

    New commit

--- index.php
+++ index.php
@@ -3,14 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>FitMode</title>
-=======
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
->>>>>>> 7a389ccb2f34b199cf266e0eca7e427e113817df
 </head>
 <body>
     <?php include './includes/_header.php'; ?>
@@ -50,8 +43,9 @@
             <a href="#" class="menu-item">Sports <i class="fas fa-chevron-down"></i></a>
             <ul class="submenu">
                 <li><a href="#">Football</a></li>
-                <li><a href="#">Tennis</a></li>
-                <li><a href="#">Natation</a></li>
+                <li><a href="#">Baskerball</a></li>
+                <li><a href="#">Handball</a></li>
+                <li><a href="#">Rugby</a></li>
             </ul>
         </li>
     </ul>
