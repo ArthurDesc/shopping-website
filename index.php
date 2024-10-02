eab03781810ba43a44425cc4c5feffa99129087c
Diomande225 <168726585+Diomande225@users.noreply.github.com>
2024-10-02 13:15:14 +0200

    New commit

--- index.php
+++ index.php
@@ -3,13 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FitMode</title>
 </head>
 <body>
     <?php include './includes/_header.php'; ?>
-    <main>
-<header></header>
-    </main>
+<div class="sidebar">
+    <ul class="menu">
+        <li>
+            <a href="#" class="menu-item">Tout les articles <i class="fas fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <!-- Sous-catégories si nécessaire -->
+            </ul>
+        </li>
+        <li>
+            <a href="#" class="menu-item">Homme <i class="fas fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <li><a href="#">T-shirts</a></li>
+                <li><a href="#">Pantalons</a></li>
+                <li><a href="#">Chaussures</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#" class="menu-item">Femme <i class="fas fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <li><a href="#">Robes</a></li>
+                <li><a href="#">Jupes</a></li>
+                <li><a href="#">Accessoires</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#" class="menu-item">Enfants <i class="fas fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <li><a href="#">Garçons</a></li>
+                <li><a href="#">Filles</a></li>
+                <li><a href="#">Bébés</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#" class="menu-item">Sports <i class="fas fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <li><a href="#">Football</a></li>
+                <li><a href="#">Tennis</a></li>
+                <li><a href="#">Natation</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>   
     <footer></footer>
 </body>
 </html>
+
+
