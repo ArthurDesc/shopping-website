274bceefef0903ba7cbaa308a340a0f132e37dfd
Derroce <arthur.descourvieres@gmail.com>
2024-10-02 12:01:38 +0200

    header

---
+++ includes/_header.php
@@ -0,0 +1,28 @@
+<header class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+  <div class="container-fluid">
+    <!-- Menu hamburger pour mobile -->
+    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <!-- Logo -->
+    <a class="navbar-brand mx-auto" href="#">
+      <img src="chemin/vers/votre/logo.png" alt="Fitmode" height="30">
+    </a>
+    
+    <!-- Icônes à droite -->
+    <div class="d-flex">
+      <a href="#" class="nav-link px-2">
+        <i class="bi bi-bag"></i>
+      </a>
+      <a href="#" class="nav-link px-2">
+        <i class="bi bi-person"></i>
+      </a>
+    </div>
+    
+    <!-- Menu de navigation (caché sur mobile) -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <!-- Ajoutez ici vos éléments de menu si nécessaire -->
+    </div>
+  </div>
+</header>
