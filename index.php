fc704d7c168ed1e14172fb94144b63f56e22d86a
Derroce <arthur.descourvieres@gmail.com>
2024-10-04 21:02:40 +0200

    carousel is moving

--- index.php
+++ index.php
@@ -3,25 +3,13 @@
 
 <?php include './includes/_header.php'; ?>
 
-<div class="swiper-container relative">
+<div class="swiper-container relative max-w-screen-xl mx-auto">
   <div class="swiper-wrapper">
-    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide1.png" alt="Image 1"></div>
-    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide2.png" alt="Image 2"></div>
-    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide3.png" alt="Image 3"></div>
+    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide1.png" alt="Image 1" class="w-full h-auto"></div>
+    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide2.png" alt="Image 2" class="w-full h-auto"></div>
+    <div class="swiper-slide"><img src="<?php echo BASE_URL; ?>assets/images/slide3.png" alt="Image 3" class="w-full h-auto"></div>
   </div>
-  <!-- Add Pagination -->
   <div class="swiper-pagination"></div>
-  <!-- Add Navigation -->
-  <div class="swiper-button-next z-10">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.75 19.5 8.25 12l7.5-7.5" />
-    </svg>
-  </div>
-  <div class="swiper-button-prev z-10">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-    </svg>
-  </div>
 </div>
 
 <section class="nouveautes py-8">
@@ -39,6 +27,23 @@
 <!-- Scripts -->
 <script src="<?php echo BASE_URL; ?>assets/js/script.js" defer></script>
 <script src="<?php echo BASE_URL; ?>assets/js/navbar.js" defer></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
+    }, 100);
+  });
+</script>
 </body>
 
 </html>
