51011016b4be86501e1a9b1fb8f13094751d4971
Derroce <arthur.descourvieres@gmail.com>
2024-10-14 14:03:13 +0200

    can add articles

--- assets/js/backoffice/categoryManager.js
+++ assets/js/backoffice/categoryManager.js
@@ -31,12 +31,7 @@ const CategoryManager = (function(UIManager) {
                 <ul id="categories-list" class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700" aria-labelledby="dropdownSearchButton">
                     <!-- Les catégories seront ajoutées ici dynamiquement -->
                 </ul>
-                <div class="p-3 border-t border-gray-200">
-                    <form id="addCategoryForm" class="flex items-center">
-                        <input type="text" id="newCategoryName" name="newCategoryName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nouvelle catégorie" required>
-                        <button type="submit" class="ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Ajouter</button>
-                    </form>
-                </div>
+
             </div>
         `;
   
