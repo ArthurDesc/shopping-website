f0046458964739ed6f0ee0abcfcad6d0d6ccdc85
abdoul-hachim <168727468+abdoul-hachim@users.noreply.github.com>
2024-10-15 13:59:34 +0200

    update de paiement

---
+++ pages/process_paiement.php
@@ -0,0 +1,31 @@
+<?php
+// Inclure l'autoloader de Composer
+require_once '../vendor/autoload.php'; // Changement effectué ici
+// Configuration de Stripe
+\Stripe\Stripe::setApiKey('sk_test_51Q7Hl1P5XJmDt2UG2j3o2mIobvzMWo0XoZ8Md4YeqakLP682h9aEuYczQfUzjEMEdt6SyLUENnbgTmZPNotX2rEa00cMDNxsLs'); // Remplace par ta clé secrète
+
+// Vérifier si le token est présent
+if (isset($_POST['Token'])) {
+    $token = $_POST['Token'];
+
+    try {
+        // Créer une charge
+        $charge = \Stripe\Charge::create([
+            'amount' => 1000, // Montant en cents (ex: 10,00 €)
+            'currency' => 'eur',
+            'description' => 'Achat dans le panier',
+            'source' => $token,
+        ]);
+        // Redirection après paiement réussi
+        header('Location: confirmation_paiement.php');
+        exit;
+    } catch (\Stripe\Exception\CardException $e) {
+        // Gestion des erreurs de carte
+        echo 'Erreur : ' . $e->getError()->message;
+    } catch (Exception $e) {
+        // Gestion des autres erreurs
+        echo 'Erreur générale : ' . $e->getMessage();
+    }
+} else {
+    echo 'Aucune information de paiement reçue.';
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f94cff3ef0385da99223217e8f0f6f25a752ddd7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f94cff3ef0385da99223217e8f0f6f25a752ddd7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v16.1.0',
+            'version' => '16.1.0.0',
+            'reference' => '47c55a5a8414d1d7584e6bbfb3e8179f3c43d83a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
