1805dca63f526dca29c7d60c707c221a03e733fe
Derroce <arthur.descourvieres@gmail.com>
2024-10-22 11:18:58 +0200

    cart work on both pages without multiply orders

--- ajax/add_to_cart.php
+++ ajax/add_to_cart.php
@@ -5,26 +5,18 @@ require_once "../classe/Panier.php";
 
 $panier = new Panier();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_produit'], $_POST['taille'], $_POST['quantite'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_produit'])) {
     $id_produit = intval($_POST['id_produit']);
-    $taille = $_POST['taille'];
-    $quantite = intval($_POST['quantite']);
-
-    if ($panier->ajouter($id_produit, $quantite, $taille)) {
-        echo json_encode([
-            'success' => true,
-            'message' => 'Produit ajouté au panier',
-            'cartCount' => $panier->getNombreArticles()
-        ]);
-    } else {
-        echo json_encode([
-            'success' => false,
-            'message' => 'Erreur lors de l\'ajout au panier'
-        ]);
-    }
+    $panier->ajouter($id_produit);
+    
+    echo json_encode([
+        'success' => true,
+        'cartCount' => $panier->getNombreArticles()
+    ]);
 } else {
     echo json_encode([
         'success' => false,
-        'message' => 'Données invalides'
+        'message' => 'Requête invalide'
     ]);
 }
+
