65370711a686091cf14c9e26bd9acd64be41b987
Derroce <arthur.descourvieres@gmail.com>
2024-10-22 16:40:05 +0200

    cann see cat and sub cat

--- pages/produit.php
+++ pages/produit.php
@@ -157,16 +157,29 @@ $marque_filter = isset($_GET['marque']) ? $_GET['marque'] : null; // Ajoutez cet
                         </div>
                         <div x-show="openTab === 'categories'" x-collapse>
                             <div class="py-4 pl-4">
-                                <?php foreach ($categories as $category): ?>
+                                <?php foreach ($categories as $id_categorie => $category): ?>
                                     <div class="flex items-center mb-2">
                                         <input type="checkbox" 
-                                               id="cat_<?php echo $category['id_categorie']; ?>" 
+                                               id="cat_<?php echo $id_categorie; ?>" 
                                                name="categories[]" 
-                                               value="<?php echo $category['id_categorie']; ?>" 
+                                               value="<?php echo $id_categorie; ?>" 
                                                class="mr-2"
-                                               <?php echo ($filtre->hasCategory($category['id_categorie'])) ? 'checked' : ''; ?>>
-                                        <label for="cat_<?php echo $category['id_categorie']; ?>"><?php echo htmlspecialchars($category['nom']); ?></label>
+                                               <?php echo ($filtre->hasCategory($id_categorie)) ? 'checked' : ''; ?>>
+                                        <label for="cat_<?php echo $id_categorie; ?>"><?php echo htmlspecialchars($category['nom']); ?></label>
                                     </div>
+                                    <?php if (!empty($category['sous_categories'])): ?>
+                                        <?php foreach ($category['sous_categories'] as $sous_categorie): ?>
+                                            <div class="flex items-center mb-2 ml-4">
+                                                <input type="checkbox" 
+                                                       id="cat_<?php echo $sous_categorie['id']; ?>" 
+                                                       name="categories[]" 
+                                                       value="<?php echo $sous_categorie['id']; ?>" 
+                                                       class="mr-2"
+                                                       <?php echo ($filtre->hasCategory($sous_categorie['id'])) ? 'checked' : ''; ?>>
+                                                <label for="cat_<?php echo $sous_categorie['id']; ?>"><?php echo htmlspecialchars($sous_categorie['nom']); ?></label>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
                                 <?php endforeach; ?>
                             </div>
                         </div>
@@ -401,4 +414,3 @@ $(document).ready(function() {
 </script>
 </body>
 </html>
-
