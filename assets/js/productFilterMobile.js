be07781df72f19bcf7c95aeae1b5bbd2c1b226c7
Derroce <arthur.descourvieres@gmail.com>
2024-10-30 11:13:39 +0100

    mobile filter form

--- assets/js/productFilterMobile.js
+++ assets/js/productFilterMobile.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const filterForm = document.getElementById('filterForm');
     const toggleFilters = document.getElementById('toggleFilters');
     const closeFilters = document.getElementById('closeFilters');
+    const applyFilters = document.getElementById('applyFilters');
 
     toggleFilters?.addEventListener('click', () => {
         filterForm.classList.add('is-active');
         document.body.style.overflow = 'hidden';
     });
 
-    closeFilters?.addEventListener('click', () => {
+    const closeFilterForm = () => {
         filterForm.classList.remove('is-active');
         document.body.style.overflow = '';
-    });
+    };
+
+    closeFilters?.addEventListener('click', closeFilterForm);
+    applyFilters?.addEventListener('click', closeFilterForm);
 });    
